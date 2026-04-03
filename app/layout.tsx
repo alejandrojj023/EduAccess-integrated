4249a4c44eb0fe8c2e9e8236aaf76edd38efa6ba
DanielGG21 <alejandrojj023@gmail.com>
2026-04-02 20:32:34 -0700

    Guarda mis últimos cambios antes de traer los de Neo

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png',  media: '(prefers-color-scheme: dark)'  },
-      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/icon.svg', type: 'image/svg+xml', sizes: 'any' },
+      { url: '/icon.svg.png', type: 'image/png', sizes: '512x512' },
     ],
-    apple: '/apple-icon.png',
+    apple: { url: '/apple-icon.png.png', sizes: '180x180' },
+    shortcut: '/icon.svg',
   },
 }
 
